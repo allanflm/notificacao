ddd1e654581ae9515e9ee0ca1c6d1c5b6df2709c
Allan <allanfelipedk@gmail.com>
2026-03-04 15:40:28 -0300

    feat: add methods of sending emails to fans

---
+++ src/main/java/com/allanfelipe/notificacao/business/dto/TarefasDTO.java
@@ -0,0 +1,32 @@
+package com.allanfelipe.notificacao.business.dto;
+
+import com.allanfelipe.notificacao.business.dto.enums.StatusNotificacaoEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TarefasDTO {
+
+    private String id;
+    private String nomeDaTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+
+    private String emailUsuario;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAlteracao;
+
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
---
+++ src/main/java/com/allanfelipe/notificacao/business/dto/enums/StatusNotificacaoEnum.java
@@ -0,0 +1,8 @@
+package com.allanfelipe.notificacao.business.dto.enums;
+
+public enum StatusNotificacaoEnum {
+    PENDENTE,
+    NOTIFICADO,
+    CANCELADO
+
+}
